6214cf6e3839fc3e52be2e71b1c4d30c73cefa4f
Saidmirzo <saidmirzobahromov88@example.com>
2024-07-02 20:37:17 +0500

    first commit

---
+++ bin/test_git.dart
@@ -0,0 +1,5 @@
+import 'package:test_git/test_git.dart' as test_git;
+
+void main(List<String> arguments) {
+  print('Hello world: ${test_git.calculate()}!');
+}
---
+++ test/test_git_test.dart
@@ -0,0 +1,8 @@
+import 'package:test_git/test_git.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
